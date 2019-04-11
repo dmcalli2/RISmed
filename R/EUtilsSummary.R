c88d3d76e8e18a09ba5d630512d0745a5580fe6f
David A McAllister <david.mcallister@glasgow.ac.uk>
2019-04-11 17:05:28 +0100

    add api key to query

--- R/EUtilsSummary.R
+++ R/EUtilsSummary.R
@@ -35,6 +35,32 @@ EUtilsSummary <- function(query,type="esearch",db="pubmed",url=NULL,encoding="un
 		)
 }
 
+EUtilsSummaryApi <- function(query,type="esearch",db="pubmed",url=NULL,encoding="unknown",...){
+
+	if(is.null(url)){
+		url <- EUtilsQuery(query,type,db,...)
+	}
+  url <- paste0(url, "&", api_key)
+	lines <- readLines(url,warn=FALSE,encoding=encoding)
+	res <- ParseTags(lines)
+	
+	EMPTYCHECK <- length(grep("<eSearchResult><Count>0<\\/Count>", lines))!=0
+	
+	if(EMPTYCHECK){
+			res$Id <- character(0)
+			res$Count <- 0
+		}
+	
+	new("EUtilsSummary",
+		db = db,
+		count = res$Count,
+		retstart = res$RetStart,
+		retmax = res$RetMax,
+		PMID = res$Id,
+		querytranslation = res$QueryTranslation
+		)
+}
+
 setMethod("print","EUtilsSummary",function(x,...) print(x@querytranslation))
 setMethod("show","EUtilsSummary",function(object) print(object@querytranslation))
 setMethod("summary","EUtilsSummary",function(object,...){
